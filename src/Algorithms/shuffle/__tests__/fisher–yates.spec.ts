18fb085ee15c5a40b60839574618768a2b76d4f0
luczod <kzod5611@gmail.com>
2023-12-24 17:04:05 -0300

    Tests: shuffle

---
+++ src/Algorithms/shuffle/__tests__/fisher–yates.spec.ts
@@ -0,0 +1,30 @@
+import { describe, expect, it } from 'vitest';
+import { shuffle } from '../fisher–yates';
+
+describe('Fisher-Yates Suffle', () => {
+  const SIZE = 100;
+
+  function createSortedArray() {
+    const array: number[] = [];
+    for (let i = 1; i <= SIZE; i++) {
+      array.push(i);
+    }
+    return array;
+  }
+
+  it('works with empty arrays', () => {
+    expect(shuffle([])).toEqual([]);
+  });
+
+  it('works with arrays with a single value', () => {
+    const array = [1];
+    expect(shuffle(array)).toEqual(array);
+  });
+
+  it('works with sorted arrays', () => {
+    let array = createSortedArray();
+    const sortedArray = createSortedArray();
+    array = shuffle(array);
+    expect(array).not.toEqual(sortedArray);
+  });
+});
